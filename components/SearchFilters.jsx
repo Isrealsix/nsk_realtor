3b638d512fe3a76a35f3c6ff164cdd6754337e16
Isreal6ix <isrealsix@gmail.com>
2021-11-27 21:07:03 +0700

    Added search components

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Image from 'next/image';
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { MdCancel } from 'react-icons/md';
+
+export default function SearchFilters() {
+  return (
+    <div>
+      SearchFilters
+    </div>
+  )
+}
