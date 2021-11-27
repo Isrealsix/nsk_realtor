2a5c724309593b6674decc221d26185fb67f29af
Isreal6ix <isrealsix@gmail.com>
2021-11-27 20:02:14 +0700

    added Layout and wrapped app with ChakraProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress';
+import { ChakraProvider } from '@chakra-ui/react';
 
+import Layout from '../components/Layout'
+import React from 'react';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+      
+      </Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </React.Fragment>
+  )
 }
 
 export default MyApp
