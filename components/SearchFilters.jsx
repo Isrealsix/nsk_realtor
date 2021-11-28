ee418af4acdb76a7bd4286854746be763f120c7e
Isreal6ix <isrealsix@gmail.com>
2021-11-29 05:37:47 +0700

    update

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,8 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/router";
-import Image from 'next/image';
-import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
-import { MdCancel } from 'react-icons/md';
+import { Flex, Select, Box } from '@chakra-ui/react';
 import { filterData, getFilterValues } from "../utils/filterData";
 
 export default function SearchFilters() {
